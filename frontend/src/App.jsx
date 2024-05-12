fca9d00dc6541599829936b5aa322526c095cf61
paramjot <pjsc204@gmail.com>
2024-05-13 02:09:15 +0530

    folder structure changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,32 @@
-import { useState } from 'react'
-import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import About from './components/About/About'
-import Contact from './components/Contact/Contact'
-import Home from './components/Home/Home'
-import Appointment from './components/Appointment/Appointment'
-import Lab from './components/Lab/Lab'
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Home from './pages/Home/Home';
+import Appointment from './pages/Appointment/Appointment';
+import Lab from './pages/Lab/Lab';
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      
-      <Navbar></Navbar>
-      <Router>
-        <Routes>
-        <Route path='/about' element={<About />} />
-      <Route path='/Appointment' element={<Appointment />} />
-      <Route path='/Home' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/Contact' element={<Contact />} />
-      <Route path='/Lab' element={<Lab />} />
-        </Routes>
-      
-      </Router>
-      
-
-    </>
-  )
+	return (
+		<>
+			<Navbar></Navbar>
+			<Router>
+				<Routes>
+					<Route path='/about' element={<About />} />
+					<Route path='/Appointment' element={<Appointment />} />
+					<Route path='/Home' element={<Home />} />
+					<Route path='/about' element={<About />} />
+					<Route path='/Contact' element={<Contact />} />
+					<Route path='/Lab' element={<Lab />} />
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
-export default App
+export default App;
