6aff6e108f494db29f8c26df0d199b22f4583c0c
paramjot <pjsc204@gmail.com>
2024-05-28 00:53:32 +0530

    mongoose added

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config({path: './.env'});
 
 const app = express();
+
+const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose.connect(DB, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false
+}).then(() => {
+	console.log('DB connection established');
+})
 
 const PORT = 3000;
 app.listen(PORT, () => {
